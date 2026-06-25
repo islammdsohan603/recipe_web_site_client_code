605a52073d38a2b7f481b1affc40f9c2b2ce721f
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-25 08:48:02 +0600

    deleted function add

--- src/components/myrecipesdata/actionbutton/DeleteButton.jsx
+++ src/components/myrecipesdata/actionbutton/DeleteButton.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
+import { toast } from 'react-toastify';
 
 const DeleteButton = ({ id }) => {
-  console.log(id);
+  const [loading, setLoading] = useState(false);
+
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+
+  const handleDelete = async () => {
+    const proceed = window.confirm(
+      'Are you sure you want to delete this recipe',
+    );
+
+    if (!proceed) return;
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `${baseUrl}/api/delete-view-recipe?id=${id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const data = await response.json();
+      if (data.success) {
+        toast.success('Recipe Delete SuccessFully!');
+
+        window.location.reload();
+      } else {
+        toast.error('Failed to Delete Recipe');
+      }
+    } catch (error) {
+      toast.warning('Something went wrong while deleting');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <button
+        onClick={handleDelete}
+        disabled={loading}
         title="Delete Recipe"
         className="p-2 flex items-center gap-2 text-red-400 bg-red-500/10 border border-red-500/20 rounded-lg cursor-pointer hover:bg-red-500/20 hover:text-red-300 transition duration-200"
       >
         <FiTrash2 size={18} />
-        delete
+        {loading ? 'deleting' : 'delte'}
       </button>
     </div>
   );
