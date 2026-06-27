75c2e4074e10a606383885499e5e4097e3f25499
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-27 19:18:38 +0600

    updata api and ui

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -1,13 +1,23 @@
-import AdminPage from '@/components/admindashboard/AdminPage';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export const AdminDashboard = async () => {
+const AdminDashboard = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session || session.user.role !== 'admin') {
+    redirect('/login');
+  }
+
   return (
     <div className="bg-[#0c0604] text-white min-h-screen p-8">
-      <h1 className="text-2xl font-bold mb-8">Welcome, Admin</h1>
-
-      <div>
-        <AdminPage />
-      </div>
+      <h1 className="text-2xl font-bold">
+        Welcome to Admin Dashboard, {session.user.name}
+      </h1>
     </div>
   );
 };
+
+export default AdminDashboard;
